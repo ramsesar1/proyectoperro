9e6e45d928999b1648a9d29bf3b5d597c30414a8
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2024-06-09 14:27:09 -0700

    hello world

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.use(function(req, res, next) {
 
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
