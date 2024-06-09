ea7a25d1f9b7bd456292bfc6120426b3bb4e6e77
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2024-06-09 14:00:06 -0700

    se modifica el backend para cors

--- server.js
+++ server.js
@@ -4,15 +4,33 @@ const cors = require('cors');
 const db = require('./src/db');
 require('dotenv').config();
 
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Use CORS middleware 
+app.use(cors()); 
+const PORT = process.env.PORT || 3001; 
+app.listen(PORT, () => { 
+  console.log("Server is running on port" ,$,{PORT}); 
+}); 
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+const express = require('express'); 
+const cors = require('cors'); 
+
+
+
+
+
+
 
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
